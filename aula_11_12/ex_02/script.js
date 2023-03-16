aad50ca549f7e49f2168f85040e2cd632e693131
Zé Mário <josemariofr@live.com>
2023-03-15 21:13:14 -0300

    Correção ex_02

--- aula_11_12/ex_02/script.js
+++ aula_11_12/ex_02/script.js
@@ -9,31 +9,44 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero = 'Homem'
             if (idade >=0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'menino.jpg')
+
             } else if (idade < 21) {
                 //Jovem
+                img.setAttribute('src', 'homem.jpg')
 
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'homem.jpg')
+
             } else {
                 //Idoso
+                img.setAttribute('src', 'idoso.jpg')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
             if (idade >=0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'menina.jpg')
             } else if (idade < 21) {
-                //Jovem
+                //Adulto
+                img.setAttribute('src', 'mulher.jpg')
             } else if (idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'mulher.jpg')
             } else {
                 //Idoso
+                img.setAttribute('src', 'idosa.jpg')
             }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos!`
+        res.appendChild(img)
     }
 }
